bdadc0e348f290c276f37b0ef8fbae34166f8578
Dushyant Sharma <Dushyant.977360@gmail.com>
2023-06-24 19:52:34 +0530

    message

---
+++ app/src/main/java/com/kaisebhi/kaisebhi/HomeNavigation/Notifications/Terms.java
@@ -0,0 +1,31 @@
+package com.kaisebhi.kaisebhi.HomeNavigation.Notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.kaisebhi.kaisebhi.R;
+
+
+public class Terms extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_terms, container, false);
+
+        return root;
+    }
+
+
+
+
+
+
+
+}
